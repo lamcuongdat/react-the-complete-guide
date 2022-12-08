718516bd3e0433dc0c9543feae451b217dd6b08d
lamcuongdat <lam.cuong.dat@gmail.com>
2022-12-08 22:32:35 +0700

    First init

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
     return (
         <div>
+            <h2>Let's get started, Lâm Cương Đạt!</h2>
             <ExpenseItem/>
         </div>
     );
