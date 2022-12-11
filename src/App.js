a847740c6314fd57896393b6c337f396de54ffb5
lamcuongdat <lam.cuong.dat@gmail.com>
2022-12-11 20:56:46 +0700

    32 - 37

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+    const expenses = [
+        {title: 'Car Insurance', amount: 294.67, date: new Date(2022, 2, 28)},
+        {title: 'Car Insurance 1', amount: 24.67, date: new Date(2022, 4, 5)},
+        {title: 'Car Insurance 2', amount: 314.67, date: new Date(2022, 2, 24)},
+        {title: 'Car Insurance 3', amount: 564.67, date: new Date(2022, 11, 14)},
+    ]
     return (
         <div>
-            <h2>Let's get started, Lâm Cương Đạt!</h2>
-            <ExpenseItem/>
+            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
+            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
+            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
+            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}/>
         </div>
     );
 }
