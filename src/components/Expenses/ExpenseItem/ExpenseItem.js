6944bbc3ad5ab64e02452ec0832b959af3c383f9
lamcuongdat <lam.cuong.dat@gmail.com>
2022-12-31 23:43:04 +0700

    60

--- src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,22 +1,15 @@
 import './ExpenseItem.css'
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from "../Card/Card";
-import {useState} from "react";
 
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title)
-    const onClick = () => {
-        setTitle('Updated')
-        console.log(title);
-    };
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={onClick}>Change title</button>
         </Card>
     );
 }
--- src/components/Expenses/Expenses/Expenses.js
+++ src/components/Expenses/Expenses/Expenses.js
@@ -1,12 +1,20 @@
 import './Expenses.css'
 import ExpenseItem from "../ExpenseItem/ExpenseItem";
 import Card from "../Card/Card";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import {useState} from "react";
 
 const Expenses = (props) => {
     const expenses = props.expenses;
+    const [yearFilter, setYearFilter] = useState(2022)
 
+    const onYearSelect = (year) => {
+        setYearFilter(parseInt(year));
+    };
     return (
         <Card className="expenses">
+            <ExpensesFilter onYearSelect={onYearSelect}
+                            currentYear={yearFilter}/>
             <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
             <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
             <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
